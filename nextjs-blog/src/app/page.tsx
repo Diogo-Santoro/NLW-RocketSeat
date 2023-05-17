dff7d5a55bffb870c746884567a0966a32b3c3c3
Diogo-Santoro <diogo.santoro05@gmail.com>
2023-05-17 02:25:00 -0300

    Commit iniciando MOBILE com EXPO

--- nextjs-blog/src/app/page.tsx
+++ nextjs-blog/src/app/page.tsx
@@ -2,5 +2,9 @@
 // JSX = Javascript + XML
 
 export default function Home() {
-  return <div className="uppercase">oi</div>
+  return (
+    <div className="h-screen bg-zinc-900 text-zinc-50">
+      <h1 className="text-4xl font-bold">Sua cápsula do tempo</h1>
+    </div>
+  )
 }
