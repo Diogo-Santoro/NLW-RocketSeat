79888b3054a4e2192922dbb24c197ba98d2837b5
Diogo-Santoro <diogo.santoro05@gmail.com>
2023-05-19 08:40:49 -0300

    Commit tentando finalizar aula 2

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,28 +1,49 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { ImageBackground, StyleSheet, Text, View } from 'react-native';
+import {} from '@expo-google-fonts/roboto'
+
+import{
+  useFonts,
+  Roboto_400Regular,
+  Roboto_700Bold,
+} from '@expo-google-fonts/roboto'
+
+import{
+  BaiJamjuree_700Bold,
+} from '@expo-google-fonts/bai-jamjuree'
+
+import blurBg from '.src/assets/bg-blur.png'
+import Stripes from './src/assets.stripes.svg'
+import {styled} from 'nativewind'
+
+const StyledStripes = styled(Stripes)
 
 export default function App() {
-  return (
-    <View className="bg-gray-950 flex-1 items-center justify-center"> 
-      <Text className='text-gray-50 font-bold text-5xl '>Hello World</Text>
-      <StatusBar style="light" translucent/>
-    </View>
-  );
-}
 
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//     backgroundColor: '#000',
-//     alignItems: 'center',
-//     justifyContent: 'center',
-//   },
-//   text:{
-//     color: '#fff',
-//     fontWeight:'700',
-//     fontSize: 40,
-//   },
-// });
+  const [hasLoadedFonts]= useFonts({
+    Roboto_400Regular,
+    Roboto_700Bold,
+    BaiJamjuree_700Bold,
+  })
+
+  if (!hasLoadedFonts){
+    return null
+  }
+
+  return (
+    <ImageBackground 
+      source={blurBg} 
+      className="relative bg-gray-900 flex-1 items-center "
+      imageStyle={{position: 'absolute', left: '-100%'}}
+    > 
+      <StyledStripes className="absolute left-2"></StyledStripes>
+
+      <View className="flex-1 items-center justify-center gap-6"></View>
+
+      <StatusBar style="light" translucent/>
+    </ImageBackground>
+  )
+}
 
 
 //CSS IN JS
